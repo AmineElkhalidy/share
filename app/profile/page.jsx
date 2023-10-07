e2ab34d846975707d17cfe1fb4b1b0291e9e9729
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-10-07 17:41:42 +0100

    Adding some features...

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
 
-const Profile = () => {
-  return <div className=""></div>;
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const [posts, setPosts] = useState([]);
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/post`);
+      const result = await response.json();
+      setPosts(result);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  return (
+    <Profile
+      name={"My"}
+      desc={"Welcome to your personalized profile page"}
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
